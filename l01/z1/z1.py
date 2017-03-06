cad05572443152eb6156ac4a36e4687375decb91
Rafal Florczak <florczak.raf@gmail.com>
2017-03-06 19:39:10 +0100

    added blacklisting, ampersand fix, comma splitting

--- l01/z1/z1.py
+++ l01/z1/z1.py
@@ -1,6 +1,10 @@
 import re
 
-ciapki = re.compile("'''([^'].*?)'''")
+BLACKLIST = ['szablon', 'uwagi', 'kategoria' 'wikipedia', 'imieniny', 'wydarzenia w', 'strona ujednoznaczniająca',
+             'lata', 'mapy i zdjęcia', 'gmina', 'liga', 'świata', 'igrzyska', 'pułk', 'brygada', 'award', 'fryderyki',
+             'oscar', 'parafia', 'ulica', 'mistrzostwa', 'wikiprojekt', 'prowincja', ':', 'nagroda', 'batalion',
+             'wybory', 'wiek']
+BOLDED = re.compile("'''([^'].*?)'''")
 
 
 def get_synonyms(header, paragraph):
@@ -12,14 +16,29 @@ def get_synonyms(header, paragraph):
     except:
         result.add(header[4:-1])
 
-
     try:
         paragraph = paragraph[:paragraph.index('(')]
     except:
         pass
 
-    result.update(ciapki.findall(paragraph))
-    return set(map(str.strip, map(str.lower, result)))
+    result.update(BOLDED.findall(paragraph))
+    result = set(map(str.lower, (map(str.strip, result))))
+
+    s = ' # '.join(result)
+    for word in BLACKLIST:
+        if word in s:
+            return set()
+
+    to_split = []
+    for word in result:
+        if ', ' in word:
+            to_split.append(word)
+
+    for word in to_split:
+        result.remove(word)
+        result.update(word.split(', '))
+
+    return result
 
 
 if __name__ == '__main__':
@@ -31,7 +50,7 @@ if __name__ == '__main__':
             if separator == '\n':
                 synonyms = get_synonyms(header, paragraph)
                 if len(synonyms) > 1:
-                    print(' # '.join(synonyms))
+                    print(' # '.join(synonyms).replace('&amp;', '&'))
             header = f.readline()
 
             while header and not header.startswith('### '):
